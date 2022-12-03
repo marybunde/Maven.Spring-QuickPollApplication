4d7176f5dbc0e934d3f29848212d513d97c2e98a
Mary Bunde <marybunde@Marys-MacBook-Air.local>
2022-12-02 20:40:31 -0500

    working on 1.3

--- src/main/java/domain/Vote.java
+++ src/main/java/domain/Vote.java
@@ -1,9 +1,17 @@
 package domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Vote {
 
 
+    @Id
+    @GeneratedValue
+    @Column(name = "VOTE_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "OPTION_ID")
+    private Option option;
 }
